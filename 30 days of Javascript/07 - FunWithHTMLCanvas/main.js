304ade0bf6e8009d805441cc16b7626db6170076
Wilson Vidyut Doloy <wilsonvidyut@gmail.com>
2022-10-19 19:56:25 +0530

    html canvas

---
+++ 30 days of Javascript/07 - FunWithHTMLCanvas/main.js
@@ -0,0 +1,57 @@
+const canvas = document.querySelector('#draw');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+ctx.strokeStyle = '#BADASS';
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
+ctx.lineWidth = 100;
+
+// ctx.globalCompositeOperation = 'multiply';
+
+let isDrawing = false; // flag
+let lastX = 0;
+let lastY = 0;
+let hue = 0;
+let direction = true;
+function draw(e) {
+	if(!isDrawing) return; // Stop the function from running when they are not moused down
+	console.log(e);
+	ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+	ctx.beginPath();
+	// Start from
+	ctx.moveTo(lastX, lastY);
+	// Go to
+	ctx.lineTo(e.offsetX, e.offsetY);
+	ctx.stroke();
+
+	// lastX = e.offsetX;
+	// lastY = e.offsetY;
+
+	//ES6 way - destructuring
+	[lastX, lastY] = [e.offsetX, e.offsetY];
+	hue++;
+	if( hue >= 360) {
+		hue = 0;
+	}
+
+	if(ctx.lineWidth >= 100 || ctx.lineWidth <= 1) {
+		direction = !direction;
+	}
+	if(direction) {
+		ctx.lineWidth++;
+	} else {
+		ctx.lineWidth--;
+	}
+
+
+
+}canvas.addEventListener('mousedown', (e) => {
+	isDrawing = true;
+	[lastX, lastY] = [e.offsetX, e.offsetY]
+
+});
+
+canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mouseup', () => isDrawing = false);
+canvas.addEventListener('mouseout', () => isDrawing = false);
